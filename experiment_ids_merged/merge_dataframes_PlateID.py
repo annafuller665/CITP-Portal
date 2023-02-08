9e371ab5df6ae499efb373b5e38bd6797b50b74d
Anna Fuller <annafuller665@gmail.com>
2023-02-08 14:00:45 -0800

    Merged ExperimentID numbers into collective data tables

---
+++ experiment_ids_merged/merge_dataframes_PlateID.py
@@ -0,0 +1,14 @@
+import pandas as pd
+# Test round
+manuals_df = pd.read_excel("/Users/phillipscitp/Desktop/CITP-Portal/Green Tea Extract/2.0_P3_manuals.xlsx")
+manuals_output = pd.read_excel("/Users/phillipscitp/Desktop/CITP-Portal/output_manuals.xlsx")
+manuals_id_num = pd.merge(manuals_df, manuals_output, on="PlateID")
+# manuals_id_num.to_excel("manuals_with_exp_id.xlsx")
+# Merge function for adding ExperimentID to all dataframes
+def merge(xlsx1: str, xlsx2: str, desired_filename: str): # xlsx1 and xlsx2 are string file paths for df conversion
+    df1 = pd.read_excel(xlsx1)
+    df2 = pd.read_excel(xlsx2)
+    df3 = pd.merge(df1, df2, on="PlateID")
+    df3.to_excel(f"{desired_filename}.xlsx")
+# merge("/Users/phillipscitp/Desktop/CITP-Portal/ALM data/ALM_NP1_PG_RVL_sorted_columns.xlsx", "/Users/phillipscitp/Desktop/CITP-Portal/output_alm.xlsx", "alm_NP1_exp_id")
+
---
+++ experiment_ids_merged/read_maunals_files.py
@@ -0,0 +1,42 @@
+
+import os
+import pandas as pd
+import re
+
+# assign directory
+directory = "manual_fm_sorted"
+
+# create a master ID table with two columns
+id_table = pd.DataFrame({"PlateID":[], "ExperimentID":[]})
+# filename number extraction
+def get_numbers_from_filename(filename):
+    return re.search(r'\d+', filename).group(0)
+
+# iterate over files in that directory
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):
+        # print the filename in the terminal
+        print(f)
+        # load the file into a pandas frame
+        excel_data_df = pd.read_excel(f, sheet_name=0)
+        unique_ids = []
+        for id in excel_data_df["PlateID"]:
+            if id not in unique_ids:
+                unique_ids.append(id)
+        # make a list of unique PlateID's that appear in the file
+        temp_df = pd.DataFrame()
+        temp_df["PlateID"] = unique_ids
+        temp_df["ExperimentID"] = get_numbers_from_filename(filename)
+        id_table = pd.concat([id_table, temp_df])
+        id_table.reset_index(drop = True)
+
+
+id_table.to_excel("output_manuals.xlsx")
+
+
+        # make a temporary data frame:
+            # first column: the list of unique PlateID's
+            # second column: the filename repeated in each row - only the number!
+        # append this temp table to the master ID table
+
